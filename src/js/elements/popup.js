400361919b0bbdf1d694140822e49ccfca0e31c8
Greg Svoboda <gregsvo@users.noreply.github.com>
2018-03-06 17:19:52 -0500

    Revert "One window"

--- src/js/elements/popup.js
+++ src/js/elements/popup.js
@@ -3,11 +3,8 @@ import Element from './elements.js';
 import { addClass, show, hide, xssEscape } from '../utils.js';
 
 const EMPTY_STRING = '';
-const TITLE_POPUP_MEMBER_LIST = 'Member List';
-const TITLE_POPUP_INVITE_LIST = 'Invite Members';
-const TITLE_POPUP_INVITE_BTN = 'Invite';
+const INVITE_POPUP_DEFAULT = -3;
 const MEMBER_POPUP_DEFAULT = -30;
-const INVITE_POPUP_DEFAULT = -1;
 const POPUP_DISTANCE = 300;
 const TITLE_POPUP_INVITE_BTN = 'Invite';
 const TITLE_POPUP_INVITE_LIST = 'Invite Members';
