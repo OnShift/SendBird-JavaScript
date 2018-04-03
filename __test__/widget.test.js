606d99a38d055e7a341fb95192d73d4f54ab566c
Sam Sawan <ssawan@onshift.com>
2018-04-03 15:10:50 -0400

    more fully test init functionality

--- __test__/widget.test.js
+++ __test__/widget.test.js
@@ -1,4 +1,10 @@
 import SBWidget from '../src/js/widget';
+import Element from '../src/js/elements/elements';
+import WidgetBtn from '../src/js/elements/widget-btn';
+import Spinner from '../src/js/elements/spinner';
+import Popup from '../src/js/elements/popup';
+import ListBoard from '../src/js/elements/list-board';
+import ChatSection from '../src/js/elements/chat-section';
 
 test('the widget can be instantiated', () => {
     let widget = new SBWidget();
@@ -6,9 +12,20 @@ test('the widget can be instantiated', () => {
 });
 
 describe('_init', () => {
-    // let widget = new SBWidget();
-    test('initializes the components we need', () => {
-        // widget._init();
-        true;
+    let widget = new SBWidget();
+    widget.widget = new Element().createDiv();
+    widget._init();
+
+    test('initializes the elelments needed for a functional chat widget', () => {
+        expect(widget.widgetBtn).toBeInstanceOf(WidgetBtn);
+        expect(widget.popup).toBeInstanceOf(Popup);
+        expect(widget.spinner).toBeInstanceOf(Spinner);
+        expect(widget.listBoard).toBeInstanceOf(ListBoard);
+        expect(widget.chatSection).toBeInstanceOf(ChatSection);
+    });
+
+    test('initializes member variables to sane empty states', () => {
+        expect(widget.activeChannelSetList).toEqual([]);
+        expect(widget.extraChannelSetList).toEqual([]);
     });
 });
