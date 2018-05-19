9421b000cbe73cd3aebcf3e53440f751b525b484
Sam Sawan <ssawan@onshift.com>
2018-05-18 21:18:30 -0400

    solidifying search input DOM in tests

--- __test__/user-management.test.js
+++ __test__/user-management.test.js
@@ -20,13 +20,28 @@ describe('createSearchBox', () => {
     let searchBox = testUserManager.createSearchBox();
 
     test('when called, sets searchImage to a div', () => {
-        expect(testUserManager.searchImage).toBeInstanceOf(HTMLDivElement)
+        let searchImage = testUserManager.searchImage;
+        expect(searchImage).toBeInstanceOf(HTMLDivElement);
+        expect(searchImage.className).toBe(className.SEARCH_IMG);
     });
 
     test('when called, sets searchInput to an editable div', () => {
         let searchInput = testUserManager.searchInput;
         expect(searchInput).toBeInstanceOf(HTMLDivElement);
         expect(searchInput.className).toContain(className.TEXT);
+        expect(searchInput.className).toContain(className.SEARCH_INPUT);
         expect(searchInput.getAttribute('contenteditable')).toBe("true");
     });
+
+    test('returns a list element', () => {
+        expect(searchBox).toBeInstanceOf(HTMLLIElement)
+    });
+
+    test('the searchBox has the appropriate children nodes', () => {
+        let searchContainer = searchBox.childNodes[0];
+        expect(searchContainer.className).toBe(className.SEARCH);
+        expect(searchContainer.childNodes[0].className).toBe(className.SEARCH_IMG);
+        expect(searchContainer.childNodes[1].className).toContain(className.TEXT);
+        expect(searchContainer.childNodes[1].className).toContain(className.SEARCH_INPUT);
+    })
 });
